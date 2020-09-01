883498d57199f86d4704ee0e5a6a958748e3dbfc
MillanUka <ukamillan@gmail.com>
2020-09-02 10:37:15 +1200

    Monor changes

--- assignment1.py
+++ assignment1.py
@@ -34,9 +34,9 @@ effects = ["No Side Effects",
 def convert_to_index(text):
     if text == "No Side Effects":
         return 0
-    elif text == "Moderate Side Effects":
-        return 1
     elif text == "Mild Side Effects":
+        return 1
+    elif text == "Moderate Side Effects":
         return 2
     elif text == "Severe Side Effects":
         return 3
@@ -47,65 +47,53 @@ test = pd.read_csv('drugLibTest_raw.tsv',delimiter='\t')    # Read the files wit
 train = pd.read_csv('drugLibTrain_raw.tsv', delimiter='\t')
 test.shape
 train.shape
-train = train.drop("Unnamed: 0", 1)
-test = test.drop("Unnamed: 0", 1)
+train.insert(1, column="review", value=None)
+train["review"] = train["benefitsReview"] + train["sideEffectsReview"]  + train["commentsReview"]
+train = train.drop(["Unnamed: 0", "benefitsReview", "sideEffectsReview", "commentsReview", "rating", "urlDrugName", "effectiveness", "condition"], axis=1)
+test["review"] = test["benefitsReview"] + test["sideEffectsReview"]  + test["commentsReview"]
+test = test.drop(["Unnamed: 0", "benefitsReview", "sideEffectsReview", "commentsReview", "rating", "urlDrugName", "effectiveness", "condition"], axis=1)
 
 #Tokenization
 tokenizer = RegexpTokenizer(r'\w+')
-train["benefitsReview"] = train["benefitsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
-train["sideEffectsReview"] = train["sideEffectsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
-train["commentsReview"] = train["commentsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
-test["benefitsReview"] = test["benefitsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
-test["sideEffectsReview"] = test["sideEffectsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
-test["commentsReview"] = test["commentsReview"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
+train["review"] = train["review"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
+test["review"] = test["review"].apply(lambda x: tokenizer.tokenize((str)(x).lower()))
 
-#Remove stopwords
-train["benefitsReview"] = train["benefitsReview"].apply(lambda x: remove_stop_words(x))
-train["sideEffectsReview"] = train["sideEffectsReview"].apply(lambda x: remove_stop_words(x))
-train["commentsReview"] = train["commentsReview"].apply(lambda x: remove_stop_words(x))
-test["benefitsReview"] = test["benefitsReview"].apply(lambda x: remove_stop_words(x))
-test["sideEffectsReview"] = test["sideEffectsReview"].apply(lambda x: remove_stop_words(x))
-test["commentsReview"] = test["commentsReview"].apply(lambda x: remove_stop_words(x))
+# Remove stopwords
+train["review"] = train["review"].apply(lambda x: remove_stop_words(x))
+test["review"] = test["review"].apply(lambda x: remove_stop_words(x))
 
-#Lemmatization
-train["benefitsReview"] = train["benefitsReview"].apply(lambda x: word_lemmatizer(x))
-train["sideEffectsReview"] = train["sideEffectsReview"].apply(lambda x: word_lemmatizer(x))
-train["commentsReview"] = train["commentsReview"].apply(lambda x: word_lemmatizer(x))
-test["benefitsReview"] = test["benefitsReview"].apply(lambda x: word_lemmatizer(x))
-test["sideEffectsReview"] = test["sideEffectsReview"].apply(lambda x: word_lemmatizer(x))
-test["commentsReview"] = test["commentsReview"].apply(lambda x: word_lemmatizer(x))
+# Lemmatization
+train["review"] = train["review"].apply(lambda x: word_lemmatizer(x))
+test["review"] = test["review"].apply(lambda x: word_lemmatizer(x))
 
-#Stemmerization
-train["benefitsReview"] = train["benefitsReview"].apply(lambda x: word_stemmer(x))
-train["sideEffectsReview"] = train["sideEffectsReview"].apply(lambda x: word_stemmer(x))
-train["commentsReview"] = train["commentsReview"].apply(lambda x: word_stemmer(x))
-test["benefitsReview"] = test["benefitsReview"].apply(lambda x: word_stemmer(x))
-test["sideEffectsReview"] = test["sideEffectsReview"].apply(lambda x: word_stemmer(x))
-test["commentsReview"] = test["commentsReview"].apply(lambda x: word_stemmer(x))
+# #Stemmerization
+train["review"] = train["review"].apply(lambda x: word_stemmer(x))
+test["review"] = test["review"].apply(lambda x: word_stemmer(x))
 
 train["sideEffects"] = train["sideEffects"].apply(lambda x: convert_to_index(x))
 test["sideEffects"] = test["sideEffects"].apply(lambda x: convert_to_index(x))
 
-train["condition"] = train['condition'].apply(lambda x: str(x)).tolist()
-test["condition"] = test['condition'].apply(lambda x: str(x)).tolist()
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
+print(train["sideEffects"])
+from sklearn.feature_extraction.text import  TfidfVectorizer, CountVectorizer, TfidfTransformer
+vectorizer = CountVectorizer(max_features=150)
+x = vectorizer.fit_transform(train["review"]).toarray()
+tfidfconverter = TfidfTransformer()
+x_train = pd.DataFrame(x, columns =vectorizer.get_feature_names())
 
-label_encode = ["urlDrugName", "condition", "benefitsReview", "sideEffectsReview", "commentsReview", "rating", "effectiveness"]
-
-for feature in label_encode:
-    train[feature] = le.fit_transform(train[feature].values)
-    test[feature] = le.fit_transform(test[feature].values)
-
-x_train = train.drop("sideEffects", 1)
+x = vectorizer.fit_transform(test["review"]).toarray()
+tfidfconverter = TfidfTransformer()
+x_test = pd.DataFrame(x, columns =vectorizer.get_feature_names())
+# train = vectorizer.fit_transform(word_count_vector) 
+# test = vectorizer.fit_transform(test)
+# x_train = train.drop("sideEffects", 1)
 y_train = train["sideEffects"]
-x_test = test.drop("sideEffects", 1)
+# x_test = test.drop("sideEffects", 1)
 y_test = test["sideEffects"]
 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 print("MLP")
 from sklearn.neural_network import MLPClassifier
-classifier = MLPClassifier(max_iter=1000)
+classifier = MLPClassifier(max_iter=500)
 classifier.fit(x_train, y_train)
 
 y_pred = classifier.predict(x_test)
